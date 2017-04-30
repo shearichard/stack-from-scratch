bd3e1c4cfcd9b33baa1380787efff0927e43b618
Richard Shea <rshea@thecubagroup.com>
2017-04-30 20:33:43 +1200

    Changes to demonstrate the use of 'React'

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,14 @@
 import 'babel-polyfill'
+/* REACT changes start ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './app'
+/* REACT changes stop  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 
-document.querySelector(APP_CONTAINER_SELECTOR).innerHTML = '<h1>Hello Webpack - Mark 2!</h1>'
+/* REACT changes start ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+// document.querySelector(APP_CONTAINER_SELECTOR).innerHTML = '<h1>Hello Webpack - Mark 2!</h1>'
+ReactDOM.render(<App />, document.querySelector(APP_CONTAINER_SELECTOR))
+/* REACT changes stop  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
